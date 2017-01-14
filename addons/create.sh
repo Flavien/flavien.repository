0f4afe159097f67352566e5a10748d15d9e61a0c
Flavien Charlon <flavien@charlon.net>
2017-01-14 16:22:15 +0000

    Add screensaver.panasonic to the repository

--- addons/create.sh
+++ addons/create.sh
@@ -1,3 +1,4 @@
 wget -O /tmp/plugin.program.jumplists.zip https://github.com/Flavien/plugin.program.jumplists/archive/master.zip
 wget -O /tmp/script.buildinstaller.zip https://github.com/Flavien/script.buildinstaller/archive/master.zip
-python create_repository.py /tmp/plugin.program.jumplists.zip /tmp/script.buildinstaller.zip ../repository.flavien
+wget -O /tmp/screensaver.panasonic.zip https://github.com/Flavien/screensaver.panasonic/archive/master.zip
+python create_repository.py /tmp/plugin.program.jumplists.zip /tmp/script.buildinstaller.zip /tmp/screensaver.panasonic.zip ../repository.flavien
